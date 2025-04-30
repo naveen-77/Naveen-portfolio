11990a7dab5f5193f880a7b4b6f382037f97c463
Naveen kumar N <93330124+naveen-77@users.noreply.github.com>
2025-04-30 16:51:20 +0530

    script.js

--- script.js
+++ script.js
@@ -91,3 +91,13 @@ document.querySelectorAll('.gallery-thumbnails img').forEach(thumb => {
         this.src = tempSrc;
     });
 });
+//new
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyXp-hU2KukjWtZjlPuemMo4R9KPPr0E8JVrooJ9jpr66MnJWeMhEsa6vfOZMioWndClA/exec'
+            const form = document.forms['google-sheet']
+          
+            form.addEventListener('submit', e => {
+              e.preventDefault()
+              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+                .catch(error => console.error('Error!', error.message))
+            })
